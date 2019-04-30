206715e9107c4ac6def33d079b5ba1395ddb3752
Matt Orvin <matthew.orvin@gmail.com>
2019-04-30 21:14:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ end
 def word_substituter(tweet)
   tweet.split.collect do |word|
     if word == dictionary.keys
-      word = dictionary.values
+      word.replace dictionary.values
 end
 end
 end
