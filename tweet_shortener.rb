c9813451e75892c8f3c023710e67f9cebaaff59e
Matt Orvin <matthew.orvin@gmail.com>
2019-04-30 20:55:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  dictionary = {
+    "hello": "hi", 
+    "to": "2", 
+    "two": "2", 
+    "too": "2", 
+    "for": "2", 
+    "four": "4", 
+    "be": "b", 
+    "you": "u", 
+    "at": "@", 
+    "and": "&"} 
+end
+
+
+def word_substituter(tweet)
+  
+end
+
+
+def bulk_tweet_shortener(tweets)
+  
+end
+
